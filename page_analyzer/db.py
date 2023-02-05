58d2d4e4ed4f19111a3d12d56ad4c260f3aa5664
a.cheremushkin <a.ch3re359@yandex.ru>
2023-02-06 00:56:51 +0300

    Delete loguru for hexlet tests

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -2,7 +2,6 @@ import os
 
 import psycopg2
 from dotenv import load_dotenv
-from loguru import logger
 
 load_dotenv()
 
@@ -17,16 +16,12 @@ class BaseDatabase:
 
     def __enter__(self):
         self.session = psycopg2.connect(self.url)
-        logger.info('im open session')
         return self.session
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if not exc_type:
-            logger.info('Im closing sesssion')
             self.session.close()
         else:
-            logger.info(f'Exc_type = {exc_type}, '
-                        f'session will be rollback and closed')
             self.session.rollback()
             self.session.close()
 
