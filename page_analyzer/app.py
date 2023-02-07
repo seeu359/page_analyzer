6c0ad97b2d868a95816ed083474193b772c5db90
a.cheremushkin <a.ch3re359@yandex.ru>
2023-02-07 18:49:10 +0300

    Minor fix

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -3,9 +3,10 @@ import os
 from flask import Flask
 
 from page_analyzer.routes import filters, main_routes
+from page_analyzer.constants import HTTP_404_NOT_FOUND
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 app.register_blueprint(main_routes.routes)
 app.register_blueprint(filters.filters)
-app.register_error_handler(404, filters.page_not_found)
+app.register_error_handler(HTTP_404_NOT_FOUND, filters.page_not_found)
